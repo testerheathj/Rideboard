2ad1e8219c4a2f4ca493720ee8e3332146146ef5
Jacob Ryan Heath <jake.heath91@gmail.com>
2013-02-19 17:39:41 -0500

    Start!
    I moved everyone's first commit to a folder we won't be using and then
    created an adroid project with eclipse. It generated all the start files
    for our project. Go ahead and sync

---
+++ App/src/rideboard/project/space/RideboardActivity.java
@@ -0,0 +1,13 @@
+package rideboard.project.space;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RideboardActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
